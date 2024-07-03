8f99335ce6293a78478aeb87d413cf5e84ef91f7
wojgie <89017406+wojgie@users.noreply.github.com>
2024-07-03 23:45:03 +0200

    creating tournaments return 201 CREATED instead of 200, also test for it

--- backend/tests/maintest.test.ts
+++ backend/tests/maintest.test.ts
@@ -2,13 +2,25 @@ import request from 'supertest';
 import { app } from '../src/main';
 
 const key = process.env.ACCESS_KEY;
+const header = `Bearer ${key}`;
 
 describe("POST /tournaments | adding tournament without ACCESS_KEY", () => {
-    it("should return 401 because of no ACCESS_KEY", async () => {
+    it("should return 401 UNAUTHORIZED because of no ACCESS_KEY", async () => {
         await request(app)
             .post("/tournaments")
             .send({ name: "Test tournament" })
             .expect('Content-Type', /json/)
             .expect(401)
+    });
+});
+
+describe("POST /tournaments | adding tournament with ACCESS_KEY", () => {
+    it("should return 201 CREATED", async () => {
+        await request(app)
+            .post("/tournaments")
+            .send({ name: "Test tournament" })
+            .expect('Content-Type', /json/)
+            .set("Authorization", header)
+            .expect(201)
     });
 });
