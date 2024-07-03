c697370f7e5436a2ee0f74c25886849d8a9b0c65
Franciszek Łopuszański <franopusz2006@gmail.com>
2024-07-04 01:48:54 +0200

    Cover tournament routes with tests

--- backend/tests/tournaments.test.ts
+++ backend/tests/tournaments.test.ts
@@ -1,39 +1,67 @@
 import request from "supertest";
 import { app } from "../src/app";
+import { clearTestData, createTestData, chess_tournament_store } from "store";
 
 const key = process.env.ACCESS_KEY;
+
 const header = `Bearer ${key}`;
+let created_code: string | undefined;
 
-describe("POST /tournaments | adding tournament without ACCESS_KEY", () => {
-	it("should return 401 UNAUTHORIZED because of no ACCESS_KEY", async () => {
-		await request(app).post("/tournaments").send({ name: "Test tournament" }).expect("Content-Type", /json/).expect(401);
+describe("Test /tournaments", () => {
+	beforeAll(() => {
+		clearTestData();
+		createTestData();
 	});
-});
+	it("Get all tournaments", async () => {
+		const res = await request(app)
+			.get("/tournaments")
+			.set("Authorization", header);
 
-let tournamentId: string;
-describe("POST /tournaments | adding tournament with ACCESS_KEY", () => {
-	beforeAll(async () => {
-		const res = await request(app).post("/tournaments").send({ name: "Test tournament" }).expect("Content-Type", /json/).set("Authorization", header).expect(201);
-		tournamentId = res.body.code;
+		expect(res.type).toEqual("application/json");
+		expect(res.status).toBe(200);
+		expect(res.body).toEqual(
+			[...chess_tournament_store.values()].map((e) => e.flatten())
+		);
 	});
+	it("Create tournament without body", async () => {
+		const res = await request(app)
+			.post("/tournaments")
+			.set("Authorization", header);
 
-	it("should return 201 CREATED when adding a game to the created tournament", async () => {
-		await request(app)
-			.post(`/tournaments/${tournamentId}/games`)
-			.send({ white_code: "9999999998", black_code: "9999999999", round: 3 })
-			.expect("Content-Type", /json/)
+		expect(res.status).toBe(400);
+	});
+	it("Create tournament", async () => {
+		const res = await request(app)
+			.post("/tournaments")
 			.set("Authorization", header)
-			.expect(201);
+			.send({ name: "Test tournament" });
+
+		expect(res.type).toEqual("application/json");
+		expect(res.status).toBe(201);
+		expect(res.body.code).toEqual(expect.any(String));
+		created_code = res.body.code;
+		expect(res.body.name).toEqual("Test tournament");
 	});
-	it("should return 201 CREATED when adding a player to the created tournament", async () => {
-		await request(app)
-			.post(`/tournaments/${tournamentId}/players`)
-			.send({ name: "name", last_name: "test-surname", rank: 42, title: "GM" })
-			.expect("Content-Type", /json/)
-			.set("Authorization", header)
-			.expect(201);
+	it("Get created tournament", async () => {
+		const res = await request(app)
+			.del("/tournaments/" + created_code)
+			.set("Authorization", header);
+
+		expect(res.type).toEqual("application/json");
+		expect(res.status).toBe(200);
+		expect(res.body.code).toEqual(created_code);
+		expect(res.body.name).toEqual("Test tournament");
 	});
-	it("should return 201 CREATED when adding a judge to the created tournament", async () => {
-		await request(app).post(`/tournaments/${tournamentId}/judges`).send({ name: "judge" }).expect("Content-Type", /json/).set("Authorization", header).expect(201);
+	it("Delete tournament", async () => {});
+	it("Get non existing tournament", async () => {
+		const res = await request(app)
+			.get("/tournaments/" + created_code)
+			.set("Authorization", header);
+
+		expect(res.type).toEqual("application/json");
+		expect(res.status).toBe(404);
+		expect(res.body).toEqual({
+			msg: "Tournament does not exist",
+		});
 	});
 });
