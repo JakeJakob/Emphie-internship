71dd5547fedf5b0c41e0331f72e44196b7d0370d
Franciszek Łopuszański <franopusz2006@gmail.com>
2024-07-09 13:40:38 +0200

    Remove dependency of test on MOCK_DATA

--- backend/tests/3judges.test.ts
+++ backend/tests/3judges.test.ts
@@ -1,47 +1,32 @@
 import request from "supertest";
 import { app, server } from "../src/app";
-import { clearTestData, createTestData, chess_tournament_store } from "store";
-import { ChessJudge, ChessTournament, EVENTS } from "types";
+import { ChessJudge, EVENTS } from "types";
 import { Socket, io } from "socket.io-client";
+import { newTournamentCode } from "./2tournaments.test";
 
 describe("Test /tournaments/:x/judges", () => {
 	const key = process.env.ACCESS_KEY;
 	const header = `Bearer ${key}`;
-	const tournament: ChessTournament =
-		chess_tournament_store.get("9999999999") || new ChessTournament("");
-	let created_code: string | undefined;
 	let socket: Socket;
+	let newJudgeCode: string | undefined;
 
 	beforeAll(() => {
 		server.listen(3000);
-		clearTestData();
-		createTestData();
-
 		socket = io("http://localhost:3000", {
 			auth: (cb) => {
-				cb({
-					token: key,
-				});
+				cb({ token: key });
 			},
 		});
 	});
+
 	afterAll(() => {
 		server.close();
 		socket.disconnect();
 	});
 
-	it("Get all judges", async () => {
-		const res = await request(app)
-			.get("/tournaments/9999999999/judges")
-			.set("Authorization", header);
-
-		expect(res.type).toEqual("application/json");
-		expect(res.status).toBe(200);
-		expect(res.body).toEqual([...tournament.judges.values()]);
-	});
 	it("Create judge without body", async () => {
 		const res = await request(app)
-			.post("/tournaments/9999999999/judges")
+			.post("/tournaments/" + newTournamentCode + "/judges")
 			.set("Authorization", header);
 
 		expect(res.status).toBe(400);
@@ -50,51 +35,60 @@ describe("Test /tournaments/:x/judges", () => {
 		socket.on(EVENTS.JUDGE_CREATED, (res) => {
 			res = JSON.parse(res) as ChessJudge;
 
-			expect(res.code).toEqual(created_code);
 			expect(res.name).toEqual("Test judge");
 		});
 
 		const res = await request(app)
-			.post("/tournaments/9999999999/judges")
+			.post("/tournaments/" + newTournamentCode + "/judges")
 			.set("Authorization", header)
 			.send({ name: "Test judge" });
 
 		expect(res.type).toEqual("application/json");
 		expect(res.status).toBe(201);
 		expect(res.body.code).toEqual(expect.any(String));
-		created_code = res.body.code;
+		newJudgeCode = res.body.code;
 		expect(res.body.name).toEqual("Test judge");
 	});
 	it("Get created judge", async () => {
 		const res = await request(app)
-			.get("/tournaments/9999999999/judges/" + created_code)
+			.get(
+				"/tournaments/" + newTournamentCode + "/judges/" + newJudgeCode
+			)
 			.set("Authorization", header);
 
 		expect(res.type).toEqual("application/json");
 		expect(res.status).toBe(200);
-		expect(res.body.code).toEqual(created_code);
 		expect(res.body.name).toEqual("Test judge");
+	});
+	it("Get all judges", async () => {
+		const res = await request(app)
+			.get("/tournaments/" + newTournamentCode + "/judges")
+			.set("Authorization", header);
+
+		expect(res.type).toEqual("application/json");
+		expect(res.status).toBe(200);
+		expect(res.body[0].name).toEqual("Test judge");
 	});
 	it("Delete judge", async () => {
 		socket.on(EVENTS.JUDGE_DELETED, (res) => {
 			res = JSON.parse(res) as ChessJudge;
 
-			expect(res.code).toEqual(created_code);
 			expect(res.name).toEqual("Test judge");
 		});
 
 		const res = await request(app)
-			.del("/tournaments/9999999999/judges/" + created_code)
+			.del(
+				"/tournaments/" + newTournamentCode + "/judges/" + newJudgeCode
+			)
 			.set("Authorization", header);
 
 		expect(res.type).toEqual("application/json");
 		expect(res.status).toBe(200);
-		expect(res.body.code).toEqual(created_code);
 		expect(res.body.name).toEqual("Test judge");
 	});
 	it("Get non existing judge", async () => {
 		const res = await request(app)
-			.get("/tournaments/9999999999/judges/" + created_code)
+			.get("/tournaments/" + newTournamentCode + "/judges/AAA")
 			.set("Authorization", header);
 
 		expect(res.type).toEqual("application/json");
--- backend/tests/3players.test.ts
+++ backend/tests/3players.test.ts
@@ -1,46 +1,33 @@
 import request from "supertest";
 import { app, server } from "../src/app";
-import { clearTestData, createTestData, chess_tournament_store } from "store";
-import { ChessPlayer, ChessTournament, EVENTS } from "types";
+import { ChessPlayer, EVENTS } from "types";
 import { Socket, io } from "socket.io-client";
+import { newTournamentCode } from "./2tournaments.test";
+
+export let newPlayerCode: string | undefined;
 
 describe("Test /tournaments/:x/players", () => {
 	const key = process.env.ACCESS_KEY;
 	const header = `Bearer ${key}`;
-	const tournament: ChessTournament =
-		chess_tournament_store.get("9999999999") || new ChessTournament("");
-	let created_code: string | undefined;
 	let socket: Socket;
 
 	beforeAll(() => {
 		server.listen(3000);
-		clearTestData();
-		createTestData();
-
 		socket = io("http://localhost:3000", {
 			auth: (cb) => {
-				cb({
-					token: key,
-				});
+				cb({ token: key });
 			},
 		});
 	});
+
 	afterAll(() => {
 		server.close();
 		socket.disconnect();
 	});
-	it("Get all players", async () => {
-		const res = await request(app)
-			.get("/tournaments/9999999999/players")
-			.set("Authorization", header);
 
-		expect(res.type).toEqual("application/json");
-		expect(res.status).toBe(200);
-		expect(res.body).toEqual([...tournament.players.values()]);
-	});
 	it("Create player without body", async () => {
 		const res = await request(app)
-			.post("/tournaments/9999999999/players")
+			.post("/tournaments/" + newTournamentCode + "/players")
 			.set("Authorization", header);
 
 		expect(res.status).toBe(400);
@@ -49,7 +36,7 @@ describe("Test /tournaments/:x/players", () => {
 		socket.on(EVENTS.PLAYER_CREATED, (res) => {
 			res = JSON.parse(res) as ChessPlayer;
 
-			expect(res.code).toEqual(created_code);
+			expect(res.code).toEqual(newPlayerCode);
 			expect(res.name).toEqual("TestName");
 			expect(res.last_name).toEqual("TestLastName");
 			expect(res.rank).toEqual(2137);
@@ -57,7 +44,7 @@ describe("Test /tournaments/:x/players", () => {
 		});
 
 		const res = await request(app)
-			.post("/tournaments/9999999999/players")
+			.post("/tournaments/" + newTournamentCode + "/players")
 			.set("Authorization", header)
 			.send({
 				name: "TestName",
@@ -69,7 +56,7 @@ describe("Test /tournaments/:x/players", () => {
 		expect(res.type).toEqual("application/json");
 		expect(res.status).toBe(201);
 		expect(res.body.code).toEqual(expect.any(String));
-		created_code = res.body.code;
+		newPlayerCode = res.body.code;
 		expect(res.body.name).toEqual("TestName");
 		expect(res.body.last_name).toEqual("TestLastName");
 		expect(res.body.rank).toEqual(2137);
@@ -77,22 +64,42 @@ describe("Test /tournaments/:x/players", () => {
 	});
 	it("Get created player", async () => {
 		const res = await request(app)
-			.get("/tournaments/9999999999/players/" + created_code)
+			.get(
+				"/tournaments/" +
+					newTournamentCode +
+					"/players/" +
+					newPlayerCode
+			)
 			.set("Authorization", header);
 
 		expect(res.type).toEqual("application/json");
 		expect(res.status).toBe(200);
-		expect(res.body.code).toEqual(created_code);
+		expect(res.body.code).toEqual(newPlayerCode);
 		expect(res.body.name).toEqual("TestName");
 		expect(res.body.last_name).toEqual("TestLastName");
 		expect(res.body.rank).toEqual(2137);
 		expect(res.body.title).toEqual("GM");
 	});
+	it("Get all players", async () => {
+		const res = await request(app)
+			.get("/tournaments/" + newTournamentCode + "/players")
+			.set("Authorization", header);
+
+		expect(res.type).toEqual("application/json");
+		expect(res.status).toBe(200);
+		expect(res.type).toEqual("application/json");
+		expect(res.status).toBe(200);
+		expect(res.body[0].code).toEqual(newPlayerCode);
+		expect(res.body[0].name).toEqual("TestName");
+		expect(res.body[0].last_name).toEqual("TestLastName");
+		expect(res.body[0].rank).toEqual(2137);
+		expect(res.body[0].title).toEqual("GM");
+	});
 	it("Delete player", async () => {
 		socket.on(EVENTS.PLAYER_DELETED, (res) => {
 			res = JSON.parse(res) as ChessPlayer;
 
-			expect(res.code).toEqual(created_code);
+			expect(res.code).toEqual(newPlayerCode);
 			expect(res.name).toEqual("TestName");
 			expect(res.last_name).toEqual("TestLastName");
 			expect(res.rank).toEqual(2137);
@@ -100,12 +107,17 @@ describe("Test /tournaments/:x/players", () => {
 		});
 
 		const res = await request(app)
-			.del("/tournaments/9999999999/players/" + created_code)
+			.del(
+				"/tournaments/" +
+					newTournamentCode +
+					"/players/" +
+					newPlayerCode
+			)
 			.set("Authorization", header);
 
 		expect(res.type).toEqual("application/json");
 		expect(res.status).toBe(200);
-		expect(res.body.code).toEqual(created_code);
+		expect(res.body.code).toEqual(newPlayerCode);
 		expect(res.body.name).toEqual("TestName");
 		expect(res.body.last_name).toEqual("TestLastName");
 		expect(res.body.rank).toEqual(2137);
@@ -113,7 +125,7 @@ describe("Test /tournaments/:x/players", () => {
 	});
 	it("Get non existing player", async () => {
 		const res = await request(app)
-			.get("/tournaments/9999999999/players/" + created_code)
+			.get("/tournaments/" + newTournamentCode + "/players/AAA")
 			.set("Authorization", header);
 
 		expect(res.type).toEqual("application/json");
