39627fd9ac25b8f4bf2fe6902ab4bcd3b3b1e04f
JakeJakob <wlostpiotr@gmail.com>
2024-07-13 02:26:56 +0200

    :tada::tada:

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { router } from "./router.tsx";
+import { router } from "./routers.tsx";
 import { RouterProvider } from "react-router-dom";
-
-export function ErrorBoundary() {
-  <div className="h-screen w-screen flex justify-center">rfdwsf</div>;
-}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
--- frontend/src/routers.tsx
+++ frontend/src/routers.tsx
@@ -5,6 +5,7 @@ import AdminPanel from "./admin-panel/admin_panel.tsx";
 import Players from "./admin-panel/players.tsx";
 import Refs from "./admin-panel/referees.tsx";
 import Error from "./Error.tsx";
+import Board from "./board/Board.tsx";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Login />, errorElement: <Error /> },
@@ -19,5 +20,9 @@ export const router = createBrowserRouter([
   {
     path: "admin_panel/referees",
     element: <Refs />,
+  },
+  {
+    path: "admin_panel/board",
+    element: <Board />,
   },
 ]);
